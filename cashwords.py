c1cc87dd636ebd8a61407e6a64523ec09ce44652
Xuanren Chen <xuanrenchen7@gmail.com>
2023-09-12 02:59:45 -0400

    Added input corrections

--- cashwords.py
+++ cashwords.py
@@ -11,7 +11,6 @@ def wordCash(word, letterList):
             return None
     return None
 
-
 def wordCashHelper(word, letterList, letterPosition):
     if word[letterPosition] in letterList:
         if letterPosition + 1 == len(word):
@@ -20,23 +19,29 @@ def wordCashHelper(word, letterList, letterPosition):
         wordCashHelper(word, letterList, letterPosition+1)
     return False
 
+def listMaker(list):
+    for i in range(1,19):
+        list.append(input(f"{i}: "))
+    corrections = input("Do you need to make any corrections? [y/n]: ")
+    while (corrections == "y"):
+        index = input("Enter the index for correction: ")
+        list[int(index) - 1] = input(f"New enter the correction for index {index}: ")
+        for i in range(18):
+            print(f"{i+1}: {list[i]}")
+        corrections = input("Correction made, do you need to make addictional corrections? [y/n]: ")
+    print()
 
-
-
-print("Cashwords puzzle solver loaded, enter all the puzzle words:")
-for i in range(1,19):
-    wordList.append(input(f"{i}: "))
-print("Now enter the the letters: ")
-for i in range(1,19):
-    letterList.append(input(f"{i}: "))
-#Expected length 18 for both
-
-
-if (len(wordList) != 18):
-    print("Word list length error")
-elif (len(letterList) != 18):
-    print("Letter list length error")
-else:
+def main():
+    print("Cashwords puzzle solver loaded...")
+    print("You will have to enter all the words and letters one by one...")
+    print("If you made a typo, you can make corrections after finish entering...")
+    print("Enter the words:")
+    listMaker(wordList)
+    print("Now enter the letter: ")
+    listMaker(letterList)
     for word in wordList:
         wordCash(word,letterList)
-    print(f'Found {len(computedList)} words: {computedList}')
+        print(f'Found {len(computedList)} words: {computedList}')
+
+
+main()
